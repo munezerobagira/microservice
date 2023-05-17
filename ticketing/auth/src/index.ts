fbebe09a11e009b80c38f21f39914246272053f1
Sostene Munezero Bagira <munezerobagira@gmail.com>
2023-05-17 10:08:10 +0200

    add the ingress controller

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 const app = express();
 app.use(express.json());
+app.get('/api/users/currentuser', (req, res) => {
+  res.send('Hi there');
+});
 app.listen(3000, () => {
   console.log('Server started on the port 3000!');
 });
