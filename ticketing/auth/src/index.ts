c91cad2b5ea144be3bf5e124db1d7bb313b0bc8b
Sostene Munezero Bagira <munezerobagira@gmail.com>
2023-05-18 16:41:02 +0200

    Fix the nginx controller configuration

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,6 +1,15 @@
 import express from 'express';
 const app = express();
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
 app.use(express.json());
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signupRouter);
+app.use(signoutRouter);
 app.get('/api/users/currentuser', (req, res) => {
   res.send('Hi there');
 });
---
+++ ticketing/auth/src/routes/current-user.ts
@@ -0,0 +1,6 @@
+import express from 'express';
+const router = express.Router();
+router.get('/api/users/current-user', (req, res) => {
+  res.send('Hi there');
+});
+export { router as currentUserRouter };
